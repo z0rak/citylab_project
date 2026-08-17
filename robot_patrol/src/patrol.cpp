9f0c475aa7d7b8ccee236fb862ab5aa207f80689
Tom Wilk <tom.wilk@gmail.com>
2026-08-16 23:50:47 -0400

    code skeleton

---
+++ robot_patrol/src/patrol.cpp
@@ -0,0 +1,40 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "rclcpp/qos.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+
+class Patrol : public rclcpp::Node
+{
+public:
+  PatrolNode(const std::string& node_name = "patrol_node")
+  : Node(node_name), node_name_(node_name)
+  {
+    auto qos = rclcpp::QoS(10).reliability(rclcpp::ReliabilityPolicy::Reliable);
+    laser_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+      "/fastbot1/scan", 10,
+      std::bind(&Patrol::laser_callback, this, std::placeholders::_1));
+
+    cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>(
+      "/fastbot_1/cmd_vel", 10);
+
+    RCLCPP_INFO(this->get_logger(), "Patrol node started");
+  }
+
+private:
+  void laser_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+  {
+    // TODO: Implement laser callback
+  
+  }
+
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<PatrolNode>());
+  rclcpp::shutdown();
+  return 0;
+}
