a2c8839eba664e6c95db8e22ad59544e8c439428
Tom Wilk <tom.wilk@gmail.com>
2026-08-19 04:19:05 +0000

    implement algorithm

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -4,11 +4,15 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include <chrono>
+#include <cmath>
+#include <mutex>
+
 using namespace std::chrono_literals;
 
 class Patrol : public rclcpp::Node {
 public:
   Patrol(const std::string &node_name = "patrol_node") : Node(node_name) {
+    
     callback_group_ = this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
     control_timer_ = this->create_wall_timer(
       std::chrono::milliseconds(100),
@@ -30,35 +34,64 @@ public:
 private:
   const double front_min_angle = -M_PI / 2.0; // -90° (right)
   const double front_max_angle = M_PI / 2.0;  // +90° (left)
+  double max_range = 0.0;
+  double best_angle = 0.0;
+
   void laser_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
-    front_ranges_.clear();
+
+    bool found = false;
     for (size_t i = 0; i < msg->ranges.size(); ++i) {
       double angle = msg->angle_min + i * msg->angle_increment;
+      double range = msg->ranges[i];
+      // is forward blocked?
+      if (std::abs(angle) < 1.8) {
+         if (range >= 35) {
+            // there are no objects closer than 35mm, keep going forward
+            return;
+         } 
+      }
       // Keep only front 180°
       if (angle < front_min_angle || angle > front_max_angle) {
         continue;
       }
-      double range = msg->ranges[i];
-
+      if (!std::isfinite(range)) {
+        continue;
+      }
       if (range < msg->range_min || range > msg->range_max) {
         continue;
       }
-      front_ranges_.push_back(range);
+      if (!found || range > max_range) {
+        max_range = range;
+        best_angle = angle;
+        found = true;
+      }
     }
-    RCLCPP_INFO(this->get_logger(), "front readings: %zu",
-                front_ranges_.size());
+
+    if (found) {
+         std::lock_guard<std::mutex> lock(scan_mutex_);
+         direction_ = best_angle;
+    }
+
   }
   
   void control_callback()
   {
-    RCLCPP_INFO(this->get_logger(), "100ms callback executed.");
-  }
+    double my_angle = 0.0;
 
+    std::lock_guard<std::mutex> lock(scan_mutex_);
+         my_angle = direction_;
+
+    auto message = geometry_msgs::msg::Twist();
+        message.linear.x = 0.1;  
+        message.angular.z = my_angle / 2;
+        cmd_vel_pub_->publish(message);
+  }
+  std::mutex scan_mutex_;
   rclcpp::CallbackGroup::SharedPtr callback_group_;
   rclcpp::TimerBase::SharedPtr control_timer_;
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_sub_;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
-  std::vector<double> front_ranges_;
+  double direction_{0.0};
 };
 
 int main(int argc, char *argv[]) {
