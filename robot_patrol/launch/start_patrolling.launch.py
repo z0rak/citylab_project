89a9615c86b89e9530df791506baff87ed772a99
Tom Wilk <tom.wilk@gmail.com>
2026-08-21 03:58:04 +0000

    update launch for RVIZ

--- robot_patrol/launch/start_patrolling.launch.py
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -1,12 +1,25 @@
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
 def generate_launch_description():
+    rviz_config = os.path.join(
+        get_package_share_directory('robot_patrol'),
+        'config',
+        'patrol.rviz'
+    )
     return LaunchDescription([
         Node(
             package='robot_patrol',
             executable='patrol',
             name='patrol_node',
+            output='screen'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config],
             output='screen'
         )
     ])
